6e46bd55fedf3a6988b2524979187fbb60cbceb4
Bethany <bethany.fowler14@gmail.com>
2017-03-05 13:51:05 +0000

    Homepage

--- eStar/Scripts/eStarJquery.js
+++ eStar/Scripts/eStarJquery.js
@@ -1,7 +1,25 @@
 ﻿$(document).ready(function () {
 
-    if ($("#selectedStudent").checked == true) {
-        console.log("checked");
-        console.log($(this).value)
-    }
+    $("#studentsIcon img").mouseover(function () {
+        $(this).attr("src", "../Content/Images/Students2.png")
+    })
+
+    $("#studentsIcon img").mouseout(function () {
+        $(this).attr("src", "../Content/Images/Students.png")
+    })
+
+    $("#classesIcon img").mouseover(function () {
+        $(this).attr("src", "../Content/Images/Classes2.png")
+    })
+
+    $("#classesIcon img").mouseout(function () {
+        $(this).attr("src", "../Content/Images/Classes.png")
+    })
+
+    $("#userIcons img").mouseover(function () {
+        $(this).attr("src", "../Content/Images/Users2.png")
+        $(".manageUsers").css("display", "block")
+    })
+
+
 })
